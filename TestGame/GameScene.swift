b76d26fad606eeb7f1d141c54b2bdf9ce11becf7
theperkinrex <theperkinrex@gmail.com>
2018-04-25 20:59:02 +0200

    Update project to make it work

--- TestGame/GameScene.swift
+++ TestGame/GameScene.swift
@@ -13,8 +13,13 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    private var testSpriteNode : SKSpriteNode?
     
     override func didMove(to view: SKView) {
+        // Init tracking area
+        let options = [NSTrackingArea.Options.mouseMoved, NSTrackingArea.Options.activeInKeyWindow] as NSTrackingArea.Options
+        let trackingArea = NSTrackingArea(rect:view.frame,options:options,owner:self,userInfo:nil)
+        view.addTrackingArea(trackingArea)
         
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
@@ -35,6 +40,12 @@ class GameScene: SKScene {
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
         }
+        
+        self.testSpriteNode = SKSpriteNode(imageNamed: "Sprite");
+        if let testSpriteNode = self.testSpriteNode {
+            testSpriteNode.scale(to: CGSize(width: 100, height: 100))
+            self.addChild(testSpriteNode)
+        }
     }
     
     
@@ -46,11 +57,17 @@ class GameScene: SKScene {
         }
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
+    func touchDragged(toPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.blue
             self.addChild(n)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint){
+        if let testSpriteNode = self.testSpriteNode {
+            testSpriteNode.position = pos
         }
     }
     
@@ -67,6 +84,10 @@ class GameScene: SKScene {
     }
     
     override func mouseDragged(with event: NSEvent) {
+        self.touchDragged(toPoint: event.location(in: self))
+    }
+    
+    override func mouseMoved(with event: NSEvent) {
         self.touchMoved(toPoint: event.location(in: self))
     }
     
@@ -87,6 +108,15 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        /*if let testSpriteNode = self.testSpriteNode {
+            //testSpriteNode.scale(to: CGSize(width: testSpriteNode.size.width + 1, height: testSpriteNode.size.height + 1))
+        }*/
+    }
+    
+    override func willMove(from view: SKView) {
+        for trackingArea in view.trackingAreas {
+            view.removeTrackingArea(trackingArea)
+        }
+        
     }
 }
