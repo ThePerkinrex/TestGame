84d600d3649c511b6a706c600f31e4083d9958aa
theperkinrex <theperkinrex@gmail.com>
2018-04-25 19:35:06 +0200

    Initial Commit

---
+++ TestGame/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TestGame
+//
+//  Created by Juan on 25/4/18.
+//  Copyright © 2018 ThePerkinrex. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
